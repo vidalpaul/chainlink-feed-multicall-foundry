8142fb02cfc2179a4b4e92637efcaa022423e911
Paulo Vidal <vidalplr@outlook.com>
2024-04-09 18:14:03 -0300

    feat: scaffold Multicall.s

--- script/Multicall.s.sol
+++ script/Multicall.s.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.24;
 
 import {Script, console} from "forge-std/Script.sol";
+import "foundry-chainlink-toolkit/script/feeds/DataFeed.s.sol";
 
 contract MulticallScript is Script {
-    function setUp() public {}
+    address public dataFeedAddress = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419;
+    address public multicallAddress =
+        0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696;
 
-    function run() public {
-        vm.broadcast();
-    }
+    // Function to get the latest 50 prices of the data feed, using multicall
+    function getLatest50() public returns (int256[] memory latestPrices) {}
 }
